7dcdf62aa52c49ccd631e58b6dbf8c98d6afb0dc
xiaothinking <2681232282@qq.com>
2021-06-11 21:12:25 +0800

    FSMC接口

---
+++ FSMC/fsmc_sram/MDK-ARM/RTE/_FSMC_IS62WV51216/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FSMC_IS62WV51216' 
+ * Target:  'FSMC_IS62WV51216' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
